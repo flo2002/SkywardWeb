ff347ee9646144f512e10c77161be9f6025ed75b
flo2002 <florian.schiemer003@gmail.com>
2022-12-16 07:53:49 +0100

    removed comments

--- src/main/java/fhv/ws22/se/skyward/webview/Controller.java
+++ src/main/java/fhv/ws22/se/skyward/webview/Controller.java
@@ -30,7 +30,6 @@ public class Controller extends HttpServlet {
     private SessionService session;
 
     public void init() {
-
         session = (SessionService) getServletContext().getAttribute("session");
 
         // Bind the SessionService to the RMI registry
@@ -44,10 +43,7 @@ public class Controller extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        PrintWriter pw = response.getWriter();
-//
-//        response.setContentType("text/html");
-//
+
         String checkIn = request.getParameter("checkin");
         String checkOut = request.getParameter("checkout");
         String room = request.getParameter("room-type");
@@ -60,30 +56,7 @@ public class Controller extends HttpServlet {
         String country = request.getParameter("country");
         String customerType = request.getParameter("customerType");
         String email = request.getParameter("email");
-//
-//        pw.println("<link rel=\"stylesheet\" href=\"stylesheet.css\" type=\"text/css\" media=\"all\">");
-//        pw.println("<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\n");
-//        pw.println("<div class=\"main\">");
-//        pw.println("<div class=\"container\">");
-//        pw.println("<table class=\"table\" border='1'>");
-//        pw.println(" <thead>\n" +
-//                "    <tr>\n" +
-//                "    </tr>\n" +
-//                "  </thead>");
-//
-//        pw.println("<tr>");
-//        pw.println("<td>"+ checkIn + "</td>");
-//        pw.println("<td>"+ checkOut + "</td>");
-//        pw.println("<td>"+ room + "</td>");
-//        pw.println("<td>"+ firstname + "</td>");
-//        pw.println("<td>"+ lastname + "</td>");
-//        pw.println("<td>"+ street + "</td>");
-//        pw.println("<td>"+ number + "</td>");
-//        pw.println("<td>"+ zip + "</td>");
-//        pw.println("<td>"+ city + "</td>");
-//        pw.println("<td>"+ country + "</td>");
-//        pw.println("<td>"+ email + "</td>");
-//        pw.println("</tr>");
+
 
         BookingDto booking = new BookingDto();
         CustomerDto customer = new CustomerDto();
@@ -119,9 +92,6 @@ public class Controller extends HttpServlet {
         session.add(customer);
         session.add(booking);
 
-
-//        pw.println("</div>");
-//        pw.println("</div>");
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
         dispatcher.forward(request,response);
     }
